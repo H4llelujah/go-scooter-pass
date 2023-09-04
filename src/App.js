cce09bf204780db6bfc72c3432fa27a78d0e3f91
Pavel Baldin <bpa25.07.2002@mail.ru>
2023-09-04 15:55:46 +0500

    AddOpenGraphOnLink

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Feature from './components/Feature';
 import Purchases from './components/Purchases';
 import Footer from './components/Footer';
 import Logo from './components/Logo';
+import OpenGraph from './img/OpenGraph.png'
+import { Helmet } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
+
+
 
 
 function App() {
@@ -24,13 +29,23 @@ function App() {
   }, []);
 
   return (
+    <HelmetProvider>
     <div className="App">
+        {/* <Helmet>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="YandexGo"/>
+          <meta property="og:url" content="https://h4llelujah.github.io/go-scooter-pass/"/>
+          <meta property="og:image" content={OpenGraph}/>
+          <meta property="og:site_name" content="YandexGo"/>
+          <meta property="og:locale" content="ru_RU"/>
+      </Helmet> */}
       <Logo></Logo>
       <Promo></Promo>
       <Feature windowWidth = {windowWidth}></Feature>
       <Purchases></Purchases>
       <Footer></Footer>   
     </div>
+    </HelmetProvider>
   );
 }
 
